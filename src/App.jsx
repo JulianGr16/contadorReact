9a4a8c9f1f22f44a159809b84f8b8cc574778129
JulianGaray16 <juliangaray.2116@gmail.com>
2024-08-27 15:45:58 -0300

    agregamos un componente y un estilo a ese componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Informacion from "./components/Informacion"
+
 
 function App() {
 
   return (
-    <>
-     <h1>HOLA MUNDO 😁</h1>
-    </>
+    <main>
+     <h1 className="text-center mb-2">Primer Proyecto React 😁</h1>
+     { /*Invocar los componentes */ }
+     <Informacion/>
+    </main>
   )
 }
 
